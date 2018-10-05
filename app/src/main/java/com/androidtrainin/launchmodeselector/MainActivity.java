da1d966e5bea1404c38497512647d05986e40e23
Sergio Fabian Aguilar Vega <sergiofav@outlook.com>
2018-10-05 01:16:33 -0400

    Add events for each button

--- app/src/main/java/com/androidtrainin/launchmodeselector/MainActivity.java
+++ app/src/main/java/com/androidtrainin/launchmodeselector/MainActivity.java
@@ -4,8 +4,11 @@ import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.Button;
 
-public class MainActivity extends AppCompatActivity {
+import java.util.EventListener;
+
+public class MainActivity extends AppCompatActivity implements View.OnClickListener {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -13,8 +16,29 @@ public class MainActivity extends AppCompatActivity {
         setContentView(R.layout.activity_main);
     }
 
-    public void btnSingleTop_onClick(View view) {
-        Intent launchIntent = getPackageManager().getLaunchIntentForPackage("com.androidtrainin.launchmodes");
+    @Override
+    public void onClick(View view) {
+        String currentPackage = "";
+
+        switch (view.getId()){
+            case R.id.btnStandard:
+                currentPackage = "com.androidtrainin.standardlaunchmode";
+                break;
+            case R.id.btnSingleTop:
+                currentPackage = "com.androidtrainin.singletoplaunchmode";
+                break;
+            case R.id.btnSingleTask:
+                currentPackage = "com.androidtrainin.singletasklaunchmode";
+                break;
+            case R.id.btnSingleInstance:
+                currentPackage = "com.androidtrainin.singleinstancelaunchmode";
+                break;
+            case R.id.btnIntentFlags:
+                currentPackage = "com.androidtrainin.intentflagslaunchmode";
+                break;
+        }
+
+        Intent launchIntent = getPackageManager().getLaunchIntentForPackage(currentPackage);
         if (launchIntent != null) {
             startActivity(launchIntent);//null pointer check in case package name was not found
         }
